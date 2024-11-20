394c52bf9cb258f0e0656d648bfc6357a9dacb65
Pablo Martins <pablomartins955@gmail.com>
2024-11-20 16:07:25 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel.Design;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+ Nokia nokia = new Nokia("123", "Flip", "123456789012345", 100);
+ Iphone iphone = new Iphone("321", "X11", "098765432109876", 120);
+
+Console.WriteLine("Ligando com os smartphones");
+ nokia.Ligar(); 
+ iphone.Ligar();
+
+Console.WriteLine("\nRecebendo ligação com os smartphones");
+nokia.ReceberLigacao();
+ iphone.ReceberLigacao();
+
+Console.WriteLine("\nInstalando Whatsapp com os smartphones");
+ nokia.InstalarAplicativo("Whatsapp");
+ iphone.InstalarAplicativo("Whatsapp");
